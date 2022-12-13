280ed4f21cc6a9c6ae91c88de0f538365fc00791
auquangtuan <auquangtuan@gmail.com>
2022-12-13 10:59:07 +0700

    Done Algorithm

---
+++ algorithm.js
@@ -0,0 +1,15 @@
+function miniMaxSum(arr) {
+  let min = arr[0];
+  let max = arr[0];
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    sum += arr[i];
+  }
+  return `${sum - max} ${sum - min}`; // or return array [sum-max, sum-min]
+}
