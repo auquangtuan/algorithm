333af639849f4e325fababbc6dd700ff6707673c
auquangtuan <auquangtuan@gmail.com>
2022-12-13 11:24:39 +0700

    add Bonus

--- algorithm.js
+++ algorithm.js
@@ -1,6 +1,8 @@
 function miniMaxSum(arr) {
   let min = arr[0];
   let max = arr[0];
+  let evend = [];
+  let odd = [];
   let sum = 0;
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] < min) {
@@ -9,7 +11,21 @@ function miniMaxSum(arr) {
     if (arr[i] > max) {
       max = arr[i];
     }
+    if (arr[i] % 2 === 0) {
+      evend.push(arr[i]);
+    } else {
+      odd.push(arr[i]);
+    }
     sum += arr[i];
   }
-  return `${sum - max} ${sum - min}`; // or return array [sum-max, sum-min]
+  const resuilt = `${sum - max} ${sum - min},
+  {
+    "Total of array ": ${sum},
+    "min in array": ${min},
+    "max in array": ${max},
+    "even elements in array": ${evend},
+    "odd elements in array": ${odd},
+  }`;
+  return resuilt;
 }
+console.log(miniMaxSum([1, 3, 5, 7, 9]));
